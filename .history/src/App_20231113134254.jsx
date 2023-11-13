42ca769c43334be88fd41252478573d028261de3
Michal Gornicki <michal.gornicki@ithouse.co>
2023-11-13 13:48:11 +0100

    1

---
+++ .history/src/App_20231113134254.jsx
@@ -0,0 +1,37 @@
+import "./App.css";
+import CssBaseline from '@mui/material/CssBaseline';
+import { Box } from "@mui/material";
+import { Route, Routes } from "react-router-dom";
+import Home from "./subpages/Home";
+import About from "./subpages/About";
+import Navbar from "./components/Navbar";
+import Subbar from "./components/Subbar";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import MessengerWidget from "./components/MessengerWidget";
+import { useMediaQuery } from "@material-ui/core";
+
+function App() {
+  const isMobile = useMediaQuery("(max-width: 700px)");
+  const theme = createTheme({
+    typography: {
+      fontFamily: "Lato, sans-serif",
+    },
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Box>
+        <Navbar isMobile={isMobile} />
+        <MessengerWidget />
+        <Routes>
+          <Route path="/" element={<Home isMobile={isMobile} />} />
+          <Route path="/about" element={<About isMobile={isMobile} />} />
+        </Routes>
+        <Subbar isMobile={isM/>
+      </Box>
+    </ThemeProvider>
+  );
+}
+
+export default App;
